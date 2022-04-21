3e4902f14f9cecbbbf90966c01204ee6364bf837
tarikkeskin <tarik.keskin007@gmail.com>
2022-04-21 22:52:32 +0300

    DataBinding-improvements

--- app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
@@ -19,33 +19,25 @@ public class MainActivity extends AppCompatActivity {
         tasarim = DataBindingUtil.setContentView(this,R.layout.activity_main);
         tasarim.setMainActivityNesnesi(this); //give permission to the object
 
-
-
-        tasarim.textViewSonuc.setText("0");
+        tasarim.setHesaplamaSonucu("0");
 
     }
 
-    public void buttonToplamaTikla(){
-        String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
-        String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
-
+    public void buttonToplamaTikla(String alinanSayi1,String alinanSayi2){
         int sayi1 = Integer.parseInt(alinanSayi1);
         int sayi2 = Integer.parseInt(alinanSayi2);
 
         int toplam = sayi1 + sayi2;
 
-        tasarim.textViewSonuc.setText(String.valueOf(toplam));
+        tasarim.setHesaplamaSonucu(String.valueOf(toplam));
     }
 
-    public void buttonCarpmaTikla(){
-        String alinanSayi1 = tasarim.editTextSayi1.getText().toString();
-        String alinanSayi2 = tasarim.editTextSayi2.getText().toString();
-
+    public void buttonCarpmaTikla(String alinanSayi1,String alinanSayi2){
         int sayi1 = Integer.parseInt(alinanSayi1);
         int sayi2 = Integer.parseInt(alinanSayi2);
 
         int carpma = sayi1 * sayi2;
 
-        tasarim.textViewSonuc.setText(String.valueOf(carpma));
+        tasarim.setHesaplamaSonucu(String.valueOf(carpma));
     }
 }
